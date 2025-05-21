2b385b983fe0b24eeb87a6171f3d77b2c79adee3
onjmin <onjmin@example.com>
2025-05-21 23:46:01 +0900

    fix

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
 	entry: ["src/index.ts"],
-	format: ["esm", "cjs", "iife"],
+	format: ["esm", "cjs"],
 	globalName: "Oekaki",
 	dts: true,
-	sourcemap: true,
+	sourcemap: false,
 	clean: true,
 	target: "es2024",
 });
